d535d8b871f3b17e881ad8a89fed0fc80085f24b
cmindy <cmindy.soft@gmail.com>
2019-12-21 21:16:46 +0900

    feat: HTTP method, header 타입 추가

---
+++ StoreApp/StoreApp/Networking/HTTP.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  StoreApp
+//
+//  Created by CHOMINJI on 2019/12/21.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import Foundation
+
+typealias HTTPHeaders = [String: String]
+
+enum HTTP {
+    enum Method: String {
+        case get = "GET"
+        case post = "POST"
+    }
+}
