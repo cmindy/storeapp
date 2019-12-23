5c0549f8d0a57aafe5937bb41903b11e7a26e049
cmindy <cmindy.soft@gmail.com>
2019-12-23 23:20:05 +0900

    fix: Router의 request를 request가 아닌 endpoint를 활용하도록 변경

--- StoreApp/StoreApp/Networking/Router.swift
+++ StoreApp/StoreApp/Networking/Router.swift
@@ -10,24 +10,22 @@ import Foundation
 
 final class Router<EndPoint: EndPointType> {
     private let decoder = JSONDecoder()
-
-    func request<T: Codable>(_ urlRequest: URLRequestConvertible, completion: @escaping (Result<T, StoreError>) -> Void) {
+    
+    func request<T: Decodable>(_ route: EndPoint, completion: @escaping (Result<T, StoreError>) -> Void) {
         let session = URLSession.shared
-        
-        guard let request = try? urlRequest.asURLRequest() else {
-            return completion(.failure(.unknown))
+        do {
+            let request = try route.asURLRequest()
+            session.dataTask(with: request) { data, response, error in
+                guard let data = data else {
+                    return completion(.failure(.requestFailed))
+                }
+                guard let model = try? JSONDecoder().decode(T.self, from: data) else {
+                    return completion(.failure(.decodeFailed))
+                }
+                completion(.success(model))
+            }.resume()
+        } catch {
+            completion(.failure(.unknown))
         }
-        
-        session.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                return completion(.failure(.requestFailed))
-            }
-            guard let model = try? JSONDecoder().decode(T.self, from: data) else {
-                return completion(.failure(.decodeFailed))
-            }
-            completion(.success(model))
-        }.resume()
     }
 }
-
-    
