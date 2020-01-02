72839fb3226559c132c2e930ca41b551c8719c66
cmindy <cmindy.soft@gmail.com>
2020-01-02 12:52:05 +0900

    chore: 네트워킹을 위한 NetworkingKit 프레임워크 파일 추가

---
+++ StoreApp/NetworkingKit/NetworkingKit.h
@@ -0,0 +1,19 @@
+//
+//  NetworkingKit.h
+//  NetworkingKit
+//
+//  Created by CHOMINJI on 2020/01/01.
+//  Copyright © 2020 cmindy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkingKit.
+FOUNDATION_EXPORT double NetworkingKitVersionNumber;
+
+//! Project version string for NetworkingKit.
+FOUNDATION_EXPORT const unsigned char NetworkingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingKit/PublicHeader.h>
+
+
