d698e8e0241659cf3216bb7b0339e7aa43cb2a66
cmindy <cmindy.soft@gmail.com>
2020-01-14 23:54:21 +0900

    chore: StoreKit 프레임워크 파일 추가

---
+++ StoreApp/StoreKit/StoreKit.h
@@ -0,0 +1,19 @@
+//
+//  StoreKit.h
+//  StoreKit
+//
+//  Created by CHOMINJI on 2020/01/14.
+//  Copyright © 2020 cmindy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoreKit.
+FOUNDATION_EXPORT double StoreKitVersionNumber;
+
+//! Project version string for StoreKit.
+FOUNDATION_EXPORT const unsigned char StoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoreKit/PublicHeader.h>
+
+
